02cadb03fc28f0c846890cf65dfc4802bb90adbb
Esteban Leon <eleon182@gmail.com>
2016-04-06 09:55:45 -0700

    init

---
+++ index.js
@@ -0,0 +1,1 @@
+module.exports = require('updash.js');
---
+++ updash.js
@@ -0,0 +1,10 @@
+var lo = require('lodash');
+
+module.exports = {
+    isSubArray: isSubArray
+};
+
+// Determines if an array is a sub array of another array
+function isSubArray(primary, secondary){
+
+}
