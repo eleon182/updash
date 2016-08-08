3db05706a42e6deb8278a0c195379f023e53e2f9
Esteban Leon <eleon182@gmail.com>
2016-08-08 13:43:39 -0700

    update unit tests

--- test/updash.spec.js
+++ test/updash.spec.js
@@ -17,3 +17,38 @@ describe('isSubArray', function(){
         expect(up.isSubArray()).toBe(false);
     });
 });
+
+describe('sizeOfArray', function(){
+    it('sizeOfArray exists', function(){
+        expect(up.sizeOfArray).toBeDefined();
+    });
+
+    it('returns 3', function(){
+        var ar = [1,2,3];
+        expect(up.sizeOfArray(ar)).toBe(3);
+    });
+});
+
+describe('cutFromArray', function(){
+    it('cutFromArray exists', function(){
+        expect(up.cutFromArray).toBeDefined();
+    });
+
+    it('returns [1,2]', function(){
+        var ar = [1,2,3];
+        expect(up.cutFromArray(ar, 2)).toEqual([1,2]);
+    });
+});
+
+describe('checkIfExists', function(){
+    it('checkIfExists exists', function(){
+        expect(up.checkIfExists).toBeDefined();
+    });
+
+    it('returns 3', function(){
+        var obj = {
+            steve: 1
+        };
+        expect(up.checkIfExists(obj, 'steve')).toEqual(true);
+    });
+});
