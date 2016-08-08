4ce608c80110be41427c453158ee9f21cf9ad405
Esteban Leon <eleon182@gmail.com>
2016-08-08 13:31:31 -0700

    add more array functions

--- updash.js
+++ updash.js
@@ -1,10 +1,39 @@
 var lo = require('lodash');
 
 lo.isSubArray = isSubArray;
+lo.sizeOfArray = sizeOfArray;
+lo.cutFromArray = cutFromArray;
+lo.checkIfExists= checkIfExists;
 
 // Determines if an array is a sub array of another array
-function isSubArray(primary, secondary){
+function isSubArray(primary, secondary) {
     return false;
+}
+
+function sizeOfArray(current) {
+    return current.length;
+}
+
+function cutFromArray(current, num) {
+    if (num > current.length) {
+        return current;
+    }
+
+    var response = [];
+    for (var i = 0; i < current.length; i++) {
+        if (i !== num) {
+            response.push(current[i]);
+        }
+    }
+    return response;
+}
+
+function checkIfExists(data, item) {
+    if (data[item] !== null && data[item] !== undefined) {
+        return true;
+    } else {
+        return false;
+    }
 }
 
 module.exports = lo;
